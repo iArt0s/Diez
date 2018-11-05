d321f7cf908efacbe12905204c74af30987eaa81
Arthem Osipov <oarthemlive@gmail.com>
2018-11-05 16:39:40 +0300

    5.11.18

--- js/script.js
+++ js/script.js
@@ -3,14 +3,40 @@
     ymaps.ready(init);
     function init(){ 
         // Создание карты.    
-        var myMap = new ymaps.Map("map", {
-            // Координаты центра карты.
-            // Порядок по умолчанию: «широта, долгота».
-            // Чтобы не определять координаты центра карты вручную,
-            // воспользуйтесь инструментом Определение координат.
-            center: [55.76, 37.64],
-            // Уровень масштабирования. Допустимые значения:
-            // от 0 (весь мир) до 19.
-            zoom: 7
+          var myMap = new ymaps.Map('map', {
+            center: [59.94199306418127,30.279423499999996],
+            zoom: 15
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+
+        myPlacemarkWithContent = new ymaps.Placemark([59.94199306418127,30.279423499999996], {
+            hintContent: 'Собственный значок метки с контентом',
+            iconContent: ''
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: 'images/icon.png',
+            // Размеры метки.
+            iconImageSize: [38, 49],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-19, -12],
+            // Смещение слоя с содержимым относительно слоя с картинкой.
+            iconContentOffset: [15, 15],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
         });
+
+        myMap.geoObjects
+        .add(myPlacemarkWithContent);
+
     }
